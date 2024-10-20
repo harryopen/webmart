f1508db98a0aed1fe0415dbd320f8c824b9eeeca
Harpreet singh <hs143297@gmail.com>
2024-10-20 13:43:06 +0530

    shopCategory

--- frontend/src/components/item/Item.jsx
+++ frontend/src/components/item/Item.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Item = (props) => {
-  console.log("The value of props",props)
+  const apiUrl = import.meta.VITE_API_URL;
   return (
-    <div className='item flex flex-col items-center justify-center text-center'>
-      <Link to={`/product/${props.id}`}> <img  onClick={window.scrollTo(0,0)}src={'http://localhost:8001'+props.image} alt={props.name} className="w-full h-auto object-contain mb-4 max-h-48 lg:max-h-full" /></Link>
+    <div className="item flex flex-col items-center justify-center text-center">
+      <Link to={`/product/${props.id}`}>
+        {' '}
+        <img
+          onClick={window.scrollTo(0, 0)}
+          src={`${apiUrl}` + props.image}
+          alt={props.name}
+          className="w-full h-auto object-contain mb-4 max-h-48 lg:max-h-full"
+        />
+      </Link>
       <p className="text-lg font-bold mb-2">{props.name}</p>
       <div className="flex justify-center items-center mb-2">
-        <div className="text-gray-700 text-2xl font-semibold">${props.new_price}</div>
-        <div className="text-gray-600 text-2xl font-medium line-through ml-2">${props.old_price}</div>
+        <div className="text-gray-700 text-2xl font-semibold">
+          ${props.new_price}
+        </div>
+        <div className="text-gray-600 text-2xl font-medium line-through ml-2">
+          ${props.old_price}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Item;
--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -1,22 +1,43 @@
-import { useContext } from 'react';
-import { ShopContext } from '../context/ShopContext';
+import { useContext, useEffect, useState } from 'react';
+
 import Item from '../components/item/Item';
 
 function ShopCategory(props) {
-  const { allProduct } = useContext(ShopContext);
-
   // Handle case when no category is provided
+  const [allProduct, setallProduct] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   const category = props.category || '';
-  console.log('Selected Category:', category);
-  console.log('All Products:', allProduct);
+
+  const fetchData = async () => {
+    try {
+      const apiUrl = `${import.meta.VITE_API_URL}${props}`;
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok ');
+      }
+      const result = response.json();
+      setallProduct(result);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   // Filter products based on category
-  const filteredProducts = allProduct.filter(item => item.category === category);
+  const filteredProducts = allProduct.filter(
+    (item) => item.category === category,
+  );
 
   return (
-    <div className='mb-[150px]'>
+    <div className="mb-[150px]">
       {props.banner && <img src={props.banner} alt="Category Banner" />}
-      <div className='popular flex flex-col items-center gap-3 h-auto'>
+      <div className="popular flex flex-col items-center gap-3 h-auto">
         <div className="popular-item grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((item, i) => (
