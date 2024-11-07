603fbb699421e611aad2876c74e9c26353a3a152
harryopen <161970330+harryopen@users.noreply.github.com>
2024-11-07 18:24:03 +0530

    update product display

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../components/BreadCrum/Breadcrum';
 import Item from '../components/item/Item';
@@ -8,19 +7,44 @@ import Description from '../components/Description/Description';
 import RelatedProducts from '../components/RelatedProducts/RelatedProducts';
 
 function Product() {
-  const {allProduct} = useContext(ShopContext);
-  const {productId}   = useParams();
-  const product       =   allProduct.find((e)=> e.id === Number(productId))
-  
+  const [allProduct, setallProduct] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { productId } = useParams();
+
+  const fetchData = async () => {
+    try {
+      const apiUrl = `${import.meta.env.VITE_API_URL}allproducts`;
+
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok ');
+      }
+      const result = await response.json();
+      setallProduct(result);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [productId]);
+  console.log("The product sent by all product route",allProduct);
+  const product =
+    allProduct && allProduct.length > 0
+      ? allProduct.find((e) => e.id === Number(productId))
+      : [];
+
   return (
     <div>
-      <Breadcrum product={product}/>
-      <ProductDisplay Product={product}/>
-      <Description/>
-      <RelatedProducts/>
-                  
+      <Breadcrum product={product} />
+      <ProductDisplay Product={product} />
+      <Description />
+      <RelatedProducts />
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
