3ba5c60f632dc4feb06a448717ee0c48e156a339
harryopen <161970330+harryopen@users.noreply.github.com>
2024-11-10 14:32:39 +0530

    Update Offer.jsx

--- frontend/src/components/offer/Offer.jsx
+++ frontend/src/components/offer/Offer.jsx
@@ -1,20 +1,22 @@
+import React from "react";
+import "./Offers.css";
+import exclusive_image from "../Assets/exclusive_image.png";
 
-import exclusive_image from '../Assets/exclusive_image.png'
-
-const Offer = () => {
+const Offers = () => {
   return (
-    <div className='offers w-3/5 min-[60vh] px-0 py-36 m-auto flex items-center mt-[150px] bg-gradient-to-b from-cyan-100 to-blue-50 ' >
-      <div className="offers-left  flex flex-1 flex-col justify-center">
-        <h1 className='text-black text-2xl font-bold'>Exclusive</h1>
-        <h1  className='text-black text-2xl font-bold'>Offers For You</h1>
-        <p  className='text-black text-xl'>ONLY ON BEST SELLERS PRODUCTS</p>
-        <button className='border-none w-32 h-5 rounded-sm bg-gray-600'>Check Now</button>
+    <div className="offers">
+      <div className="offers-left">
+        <h1>Exclusive</h1>
+        <h1>Offers For You</h1>
+        <p>ONLY ON BEST SELLERS PRODUCTS</p>
+        <button>Check now</button>
       </div>
-      <div className="offers-right flex flex-1 item-center justify-end">
+      <div className="offers-right">
         <img src={exclusive_image} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Offer
+export default Offers;
+
