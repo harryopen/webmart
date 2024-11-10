28a38d22287ca5cd7ffd5d5695466612c4487f09
harryopen <161970330+harryopen@users.noreply.github.com>
2024-11-10 14:49:06 +0530

    Update Popular.jsx

--- frontend/src/components/popular/Popular.jsx
+++ frontend/src/components/popular/Popular.jsx
@@ -20,19 +20,13 @@ function Popular() {
         {popularWomen ? (
           popularWomen.map((item, i) => {
             return (
-              <Item
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            );
-          })
-        ) : (
-          <></>
-        )}
+             <div className='popular'>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr />
+      <div className="popular-item">
+        {props.data.map((item,index)=>{
+            return <Item id={item.id} key={index} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price}/>
+        })}
       </div>
     </div>
   );
