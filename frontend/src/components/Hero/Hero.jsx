11f0f9e0385712323c5d1dd83d3310ac1e96cf2b
harryopen <161970330+harryopen@users.noreply.github.com>
2024-11-10 14:25:12 +0530

    Update Hero.jsx

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,27 +1,27 @@
-
-import hero_image from "../Assets/hero_image.png";
-import hand_icon from "../Assets/hand_icon.png";
-import arrow_icon from "../Assets/arrow.png";
+import hero_image from '../Assets/hero_image.png';
+import hand_icon from '../Assets/hand_icon.png';
+import arrow_icon from '../Assets/arrow.png';
+import './Hero.css';
 
 const Hero = () => {
   return (
-    <div className="hero flex h-[100vh] bg-gradient-to-b from-cyan-100 to-blue-50">
-      <div className="hero-left  flex-1 flex flex-col justify-center gap-5 pl-[180px]">
-        <h2 className="text-2xl font-bold">NEW ARRIVALS ONLY</h2>
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
         <div>
-          <div className=" hero-hand-icon flex item-center gap-10">
-            <p  className="text-3xl font-extrabold">new</p>
-            <img className="w-32 " src={hand_icon} alt="" />
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
           </div>
-          <p className="text-3xl font-extrabold">collections</p>
-          <p className="text-3xl font-extrabold">for everyone</p>
+          <p>collections</p>
+          <p>for everyone</p>
         </div>
-        <div className="hero-latest-btn flex justify-center items-center gap-5 w-40 h-10 rounded-sm text-2xl">
+        <div className="hero-latest-btn">
           <div>Latest Collection</div>
           <img src={arrow_icon} alt="" />
         </div>
       </div>
-      <div className="hero-right flex flex-1 item-center justify-center">
+      <div className="hero-right">
         <img src={hero_image} alt="hero" />
       </div>
     </div>
@@ -29,3 +29,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
